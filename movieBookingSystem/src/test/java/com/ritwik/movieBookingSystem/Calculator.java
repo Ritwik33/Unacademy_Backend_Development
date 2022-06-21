fe79f4e89d32bab0a59a71e8a3fb8e246786f196
Ritwik33 <ritwikawesome556@gmail.com>
2022-06-21 23:52:02 +0530

    basic testing of calculator class

---
+++ movieBookingSystem/src/test/java/com/ritwik/movieBookingSystem/Calculator.java
@@ -0,0 +1,20 @@
+package com.ritwik.movieBookingSystem;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int sub(int a, int b) {
+        return a-b;
+    }
+
+    public int mult(int a, int b) {
+        return a*b;
+    }
+
+    public double div(int a, int b) {
+        return (double)((double)a/(double)b);
+    }
+}

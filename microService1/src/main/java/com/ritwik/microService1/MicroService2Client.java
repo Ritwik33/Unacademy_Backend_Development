7fb532ab10a98f4f717be6c52bab91de57875cf6
Ritwik33 <ritwikawesome556@gmail.com>
2022-08-14 12:17:17 +0530

    ...

--- microService1/src/main/java/com/ritwik/microService1/MicroService2Client.java
+++ microService1/src/main/java/com/ritwik/microService1/MicroService2Client.java
@@ -1,2 +1,26 @@
-package com.ritwik.microService1;public class MicroService2Client {
+package com.ritwik.microService1;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * this class will be responsible for communicating to microService2
+ */
+
+//@FeignClient(value = "myFeignClient", url = "http://localhost:7072")
+@FeignClient(value = "MICROSERVICE2")
+public interface MicroService2Client {
+
+    /**
+     *
+     * this method should have the logic to call the microService2
+     *
+     * No implementation needed, so use interface not a class ...
+     *
+     * @return
+     */
+
+    @GetMapping(value = "/ms2/v1/messages")
+    public String getMessage();
+
 }
